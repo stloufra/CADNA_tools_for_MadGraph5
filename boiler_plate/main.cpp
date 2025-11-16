0a08746cb6def54eb3a4fc1c7a41bd992f94bfd0
stloufra <frantisek.stloukal@cern.ch>
2025-11-16 21:54:01 +0100

    begin of boiler-plate for CPPProcess.cpp

---
+++ boiler_plate/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "src/CPPProcess.h"
+
+int main()
+{
+
+    const int nevt = 24;
+    //what is coupling and is it changing or decided upon momenta?
+
+
+
+    return 0;
+
+}
